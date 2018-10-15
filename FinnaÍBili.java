467cc191ffb62317b714e9ba7773d61214d3ccd8
Enar Kornelius Leferink <enar@tg-dw162.rhi.hi.is>
2018-10-15 12:22:52 +0000

    Tímadæmi 7

---
+++ FinnaÍBili.java
@@ -0,0 +1,17 @@
+public class FinnaÍBili{
+  public static int finna(int[] a, int x){
+    int sæti = -1;
+    for (int i=0;i<a.length;i++){
+      if (a[i]==x) sæti = i;
+    }
+    return sæti;
+  }
+  public static void main(String[] args) {
+    int[] a = new int [10];
+    for (int i=0;i<10;i++){ //fylkið sett -slembifylki
+      a[i] = (int)(Math.random()*10)+1;
+    }
+    StdOut.println("2 er í sæti "+finna(a, 2));
+    StdOut.println("15 er í sæti "+finna(a, 15));
+  }
+}
