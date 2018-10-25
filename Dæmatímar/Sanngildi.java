40a5ad592d8cca274b7be723766041765d57b458
Enar Kornelius Leferink <enar@Enars-MacBook-Pro.local>
2018-10-25 17:08:12 +0000

    uppfærsla

--- Dæmatímar/Sanngildi.java
+++
@@ -1,9 +0,0 @@
-public class Sanngildi {  
-    public static void main(String[] args) {  
-        boolean sanngildi = Math.sqrt(2.0)*Math.sqrt(2.0)==2.0;
-        double utkoma = Math.sqrt(2.0)*Math.sqrt(2.0);
-        System.out.println(sanngildi);
-        System.out.println("Það kemur ekki 2 út eins og maður mætti halda því Math.sqrt er námundun við óræðu töluna. " 
-        + "Þetta margfeldi verður því ekki nákvæmt. Margfeldið var " + utkoma + ". ");
-    }  
-}  
